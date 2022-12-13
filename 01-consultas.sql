0d8273a9e63cb207d13c91c1f730f65a8d7c1b6c
wesleysousa00 <wesleysousa00>
2022-12-13 00:58:25 +0000

    teste

--- 01-consultas.sql
+++ 01-consultas.sql
@@ -1,12 +1,16 @@
 -- Databricks notebook source
 -- MAGIC %sql
 -- MAGIC SELECT 
--- MAGIC A.*,
--- MAGIC B.*
+-- MAGIC   A.*,
+-- MAGIC   B.*
 -- MAGIC FROM silver_olist.item_pedido A
 -- MAGIC INNER JOIN silver_olist.avaliacao_pedido B
--- MAGIC ON A.idPedido = B.idPedido
+-- MAGIC         ON A.idPedido = B.idPedido
 -- MAGIC WHERE A.idPedidoItem = 1
+
+-- COMMAND ----------
+
+SELECT 'OLÁ MUNDO'
 
 -- COMMAND ----------
 
